43c836d03bef3d0e179000efcb14271a917c150c
Matt Hinchliffe <matt@maketea.co.uk>
2012-05-16 12:56:26 +0100

    Added `hoverPause` and `autostop` options. Added `.play()` and `.stop()` methods to control autoplay. Queue option deprecated in favour of less obtuse `jumpQueue` option. Minor formatting changes.

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,8 +1,8 @@
 ﻿/**
  * @name        jQuery Slideshow
  * @author      Matt Hinchliffe <https://github.com/i-like-robots/jQuery-Slideshow>
- * @modified    10/05/2012
- * @version     1.1.2
+ * @modified    15/05/2012
+ * @version     1.2.0
  * @description jQuery Slideshow
  * @example
  * <div class="slideshow">
@@ -22,20 +22,21 @@
 	"use strict";
 
 	var defaults = {
+		carousel: '.carousel',    // Selector for the carousel element.
+		items: '.slide',          // Selector for carousel items.
 		auto: 6000,               // Autoplay timeout in milliseconds. Set to false for no autoplay.
-		speed: 600,               // Animation speed between slides in milliseconds.
-		carousel: '.carousel',    // Selector for the carousel element
-		items: '.slide',          // Selector for carousel items
+		autostop: true,           // Stop autoplay when user manually changes slide.
+		hoverPause: false,        // Pause autoplay on hover.
 		easing: 'swing',          // Animation easing for single transition
 		easeIn: 'swing',          // Animation easing on fade in.
 		easeOut: 'swing',         // Animation easing on fade out.
+		speed: 600,               // Animation speed between slides in milliseconds.
 		pagination: true,         // Render pagination.
 		skip: true,               // Render next/previous skip buttons.
-		queue: false,             // Allow .to() method while animations are queued.
+		jumpQueue: true,          // Allow .to() method while animations are queued.
 		loop: false,              // Allow slideshow to loop.
 		transition: 'scroll',     // Specify transition.
 		gestures: true,           // Allow touch swipe events to control previous/next.
-
 		onupdate: undefined       // A callback function to execute on update event.
 	};
 
@@ -43,7 +44,7 @@
 	{
 		this.target = target;
 		this.$target = $(target);
-		this.opts = $.extend({}, defaults, options, this.$target.data()); // Create a new options object for each instance
+		this.opts = $.extend( {}, defaults, options, this.$target.data() ); // Create a new options object for each instance
 
 		this._init();
 
@@ -79,12 +80,12 @@
 
 			this._transitions[ this.opts.transition ].setup.call(this);
 
-			// Create controls
-			if (this.opts.pagination)
+			// Create pagination controls
+			if ( this.opts.pagination )
 			{
 				this.$pagination = $('<ul class="slides-pagination" />');
 
-				for (var i = 0; i < this.count; i++)
+				for ( var i = 0; i < this.count; i++ )
 				{
 					$('<li class="' + ( i === this.current ? 'selected' : '' ) + '">' +
 						'<a data-slides="' + i + '" href="">' + (i + 1) + '</a>' +
@@ -94,14 +95,15 @@
 				this.$pagination.appendTo( this.$target );
 			}
 
-			if (this.opts.skip)
+			// Create skip link controls
+			if ( this.opts.skip )
 			{
 				this.$next = $('<a class="slides-next" data-slides="next" href="">Next</a>').appendTo( this.$target );
 				this.$previous = $('<a class="slides-previous" data-slides="previous" href="">Previous</a>').appendTo( this.$target );
 			}
 
-			// Events
-			if (this.opts.pagination || this.opts.skip)
+			// Control events
+			if ( this.opts.pagination || this.opts.skip )
 			{
 				this.$target.on('click.slides', '[data-slides]', function(e)
 				{
@@ -109,19 +111,27 @@
 
 					var $this = $(this);
 
-					if ( ! $this.hasClass('disabled'))
+					if ( ! $this.hasClass('disabled') )
 					{
 						self.to( $this.data('slides') );
 
 						// Stop autoplay
 						if (self.timeout)
 						{
-							clearTimeout( self.timeout );
+							if ( self.opts.autostop )
+							{
+								self.stop();
+							}
+							else
+							{
+								self.play();
+							}
 						}
 					}
 				});
 			}
 
+			// On update
 			this.$target.on('update.slides', function()
 			{
 				self._update();
@@ -156,7 +166,7 @@
 					{
 						var dir = function()
 						{
-							if (Math.abs(self.t.x1 - self.t.x2) >= Math.abs(self.t.y1 - self.t.y2))
+							if ( Math.abs(self.t.x1 - self.t.x2) >= Math.abs(self.t.y1 - self.t.y2) )
 							{
 								return self.t.x1 - self.t.x2 > 0 ? 'left' : 'right';
 							}
@@ -176,9 +186,16 @@
 						}
 
 						// Stop autoplay
-						if (self.timeout)
+						if ( self.timeout )
 						{
-							clearTimeout(self.timeout);
+							if ( self.opts.autostop )
+							{
+								self.stop();
+							}
+							else
+							{
+								self.play();
+							}
 						}
 					}
 				}, false);
@@ -187,18 +204,27 @@
 			this.to(this.current);
 
 			// Autoplay
-			if (this.opts.auto)
+			if ( this.opts.auto )
 			{
-				this.timeout = setInterval(function()
+				if ( this.opts.hoverPause )
 				{
-					self.to( self.current + 1 );
-				}, this.opts.auto);
+					this.$target.hover(function()
+					{
+						self.stop();
+					},
+					function()
+					{
+						self.play();
+					});
+				}
+
+				this.play();
 			}
 		},
 
 		_update: function()
 		{
-			if (this.opts.pagination)
+			if ( this.opts.pagination )
 			{
 				this.$pagination
 					.children()
@@ -207,7 +233,7 @@
 					.addClass('selected');
 			}
 
-			if (this.opts.skip && ! this.opts.loop)
+			if ( this.opts.skip && ! this.opts.loop )
 			{
 				if ( this.hasNext() )
 				{
@@ -229,7 +255,7 @@
 			}
 
 			// Callback
-			if (this.opts.onupdate)
+			if ( this.opts.onupdate )
 			{
 				this.opts.onupdate.call(this, this.current);
 			}
@@ -254,16 +280,21 @@
 				},
 				execute: function(to)
 				{
-					var $next = this.$items.eq(to).css('position', 'absolute').fadeIn(this.opts.speed, this.opts.easeIn);
+					var $next = this.$items
+						.eq(to)
+						.css('position', 'absolute')
+						.fadeIn(this.opts.speed, this.opts.easeIn);
 
-					this.$items.eq(this.current).fadeOut(this.opts.speed, this.opts.easeOut, function()
-					{
-						$next.css('position', 'static');
-					});
+					this.$items
+						.eq(this.current)
+						.fadeOut(this.opts.speed, this.opts.easeOut, function()
+						{
+							$next.css('position', 'static');
+						});
 				},
 				teardown: function()
 				{
-					this.$items.removeAttr('style');
+					this.$items.stop(true, true).removeAttr('style');
 				}
 			},
 			scroll: {
@@ -289,7 +320,7 @@
 					this.count = this.realcount;
 					delete this.realcount;
 
-					this.$carousel.removeAttr('style');
+					this.$carousel.stop(true, true).removeAttr('style');
 					this.$items.removeAttr('style');
 				}
 			}
@@ -297,12 +328,12 @@
 
 		hasNext: function()
 		{
-			return (this.current === this.count - 1);
+			return this.current === this.count - 1;
 		},
 
 		hasPrevious: function()
 		{
-			return (this.current === 0);
+			return this.current === 0;
 		},
 
 		// Next
@@ -320,14 +351,18 @@
 		// Go to x
 		to: function(x)
 		{
-			// If current slide is animating ignore the request,
-			if ( ! this.opts.queue && this.$items.queue('fx').length ) // <http://jsperf.com/animated-pseudo-selector/3>
+			// Allow method while animating?
+			if ( this.opts.jumpQueue )
+			{
+				this.$items.stop(true, true);
+			}
+			else if ( this.$items.queue('fx').length ) // <http://jsperf.com/animated-pseudo-selector/3>
 			{
 				return;
 			}
 
 			// Shortcuts
-			if (x === 'next')
+			if ( x === 'next' )
 			{
 				x = this.current + 1;
 			}
@@ -337,19 +372,19 @@
 			}
 
 			// Loop
-			if (x > this.count - 1)
+			if ( x > this.count - 1 )
 			{
 				if ( ! this.opts.loop)
 				{
-					clearInterval( this.timeout );
+					this.stop();
 					return;
 				}
 
 				x = 0;
 			}
-			else if (x < 0)
+			else if ( x < 0 )
 			{
-				if ( ! this.opts.loop)
+				if ( ! this.opts.loop )
 				{
 					return;
 				}
@@ -357,7 +392,7 @@
 				x = this.count - 1;
 			}
 
-			if (x !== this.current)
+			if ( x !== this.current )
 			{
 				this._transitions[ this.opts.transition ].execute.call(this, x);
 				this.current = x;
@@ -371,7 +406,7 @@
 		{
 			this._transitions[ this.opts.transition ].teardown.call(this);
 
-			if (transition)
+			if ( transition )
 			{
 				this.opts.transition = transition;
 			}
@@ -379,7 +414,25 @@
 			this._transitions[ this.opts.transition ].setup.call(this);
 
 			this.to(0);
+		},
+
+		// Start autoplay
+		play: function()
+		{
+			var self = this;
+
+			this.timeout = setInterval(function()
+			{
+				self.to( self.current + 1 );
+			}, this.opts.auto);
+		},
+
+		// Stop autoplay
+		stop: function()
+		{
+			clearInterval( this.timeout );
 		}
+
 	};
 
 	// jQuery plugin wrapper
