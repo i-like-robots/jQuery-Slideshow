f27aeafa8cc88479e41ec846c5bdf9cf3eb87bd4
i-like-robots <matt@maketea.co.uk>
2014-03-13 17:03:41 +0000

    Update dependencies and tooling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,40 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('slideshow.jquery.json'),
-    uglify: {
-      options: {
-        banner: [
-        '/*!',
-        ' * @name        <%= pkg.title %>',
-        ' * @author      <%= pkg.author.name %> <<%= pkg.homepage %>>',
-        ' * @modified    <%= grunt.template.today("dddd, mmmm dS, yyyy, HH:MM:ss") %>',
-        ' * @version     <%= pkg.version %>',
-        ' */'].join('\n')
-      },
-      my_target: {
-        files: {
-          'dist/slideshow.js': ['<banner>', 'src/slideshow.js']
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('slideshow.jquery.json'),
+        uglify: {
+            options: {
+                banner: [
+                '/*!',
+                ' * @name        <%= pkg.title %>',
+                ' * @author      <%= pkg.author.name %> <<%= pkg.homepage %>>',
+                ' * @modified    <%= grunt.template.today("dddd, mmmm dS, yyyy, HH:MM:ss") %>',
+                ' * @version     <%= pkg.version %>',
+                ' */'].join('\n')
+            },
+            my_target: {
+                files: {
+                    'dist/slideshow.js': ['<banner>', 'src/slideshow.js']
+                }
+            }
+        },
+        jshint: {
+            all: ['Gruntfile.js', 'src/*.js'],
+            options: {
+                trailing: true,
+                browser: true,
+                globals: {
+                    jQuery: true,
+                    define: false,
+                    module: true
+                }
+            }
         }
-      }
-    },
-    jshint: {
-      all: ['Gruntfile.js', 'src/*.js'],
-      options: {
-        trailing: true,
-        browser: true,
-        globals: {
-          jQuery: true,
-          define: false,
-          module: true
-        }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
